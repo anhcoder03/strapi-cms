4eca1f5b2e886575cabc710c8bf4aed31ac64dd9
anhcoder03 <anhcoder03@example.com>
2023-10-09 15:35:30 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,39 +1,14 @@
-const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('database-1.c12xdhhlw9uw.ap-southeast-2.rds.amazonaws.com', 'database-1.c12xdhhlw9uw.ap-southeast-2.rds.amazonaws.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('database-1', 'database-1'),
-        user: env('phianh', 'phianh'),
-        password: env('Anh16102003', 'Anh16102003'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
